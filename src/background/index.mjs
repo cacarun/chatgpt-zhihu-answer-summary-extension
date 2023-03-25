61b1daa5114a929dfe3bb9efee8f7073bcac0655
Cai JiaWei <1041028231@qq.com>
2023-03-25 16:11:16 +0800

    chatGPT zhihu answer

--- src/background/index.mjs
+++ src/background/index.mjs
@@ -2,10 +2,21 @@ import ExpiryMap from "expiry-map";
 import { v4 as uuidv4 } from "uuid";
 import Browser from "webextension-polyfill";
 import { fetchSSE } from "./fetch-sse.mjs";
+import cheerio from "cheerio";
 
 const KEY_ACCESS_TOKEN = "accessToken";
 
 const cache = new ExpiryMap(10 * 1000);
+
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+  'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+};
+
+var zhUrl = "";
+var zhTitle = "";
+var zhSubTitle = "";
+var zhAnswerDict = {};
 
 async function getAccessToken() {
   if (cache.get(KEY_ACCESS_TOKEN)) {
@@ -82,11 +93,69 @@ async function getAnswer(port, question) {
   });
 }
 
+async function requestZhihuData() {
+  try {
+    const response = await fetch(zhUrl, { headers });
+    const html = await response.text();
+
+    const loadHtml = cheerio.load(html);
+    const scriptContent = loadHtml('#js-initialData').html();
+    const initialData = JSON.parse(scriptContent);
+    const initialState = initialData.initialState;
+
+    const questionId = zhUrl.match(/\/question\/(\d+)/)[1];
+
+    console.log('[requestZhihuData] initialData:', initialData, 'questionId:', questionId);
+
+    var title = initialState.entities.questions[questionId].title;
+    if (title && title.trim().length > 0) {
+      zhTitle = title;
+    }
+    var subTitle = initialState.entities.questions[questionId].detail;
+    if (subTitle && subTitle.trim().length > 0) {
+      console.log('[requestZhihuData] before subTitle:', subTitle);
+      zhSubTitle = cheerio.load(subTitle).text();
+    }
+    console.log('[requestZhihuData] title:', zhTitle, ' subTitle:', zhSubTitle);
+
+    const answers = initialState.entities.answers;
+    Object.keys(answers).forEach((key) => {
+      var answer = answers[key].content
+      console.log('[requestZhihuData] before answer:', answer);
+      answer = cheerio.load(answer).text();
+      console.log('[requestZhihuData] after answer:', answer);
+      zhAnswerDict[key] = answer;
+    });
+    console.log('[requestZhihuData] zhAnswerDict:', zhAnswerDict);
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
 Browser.runtime.onConnect.addListener((port) => {
   port.onMessage.addListener(async (msg) => {
     console.log("received msg", msg);
+
+    zhUrl = msg.url
+    zhTitle = msg.title;
+    zhSubTitle = msg.subTitle;
+    zhAnswerDict = {};
+
+    await requestZhihuData()
+    // await getTopAnswers(msg.url).then((topAnswers) => {
+    //   console.log(topAnswers);
+    // });
+
+    var input = ""
+    if (msg.type === 'answer') {
+      input = `${zhTitle} ${zhSubTitle}`
+    } else if (msg.type === 'summary') {
+
+    }
     try {
-      await getAnswer(port, msg.question);
+      await getAnswer(port, input);
     } catch (err) {
       console.error("catch error", err);
       port.postMessage({ error: err.message });
@@ -94,3 +163,5 @@ Browser.runtime.onConnect.addListener((port) => {
     }
   });
 });
+
+
